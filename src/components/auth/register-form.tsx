2e67e251f553016fa1d5c0fc4c791f65a45261f5
Ashutosh Gupta <ashutoshg479@gmail.com>
2024-05-28 10:37:17 +0530

    forms completed

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -12,7 +12,7 @@ import { CardWrapper } from "./card-wrapper";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 import { Input } from "@/components/ui/input";
 import { FaEye } from "react-icons/fa";
 import { Button } from "../ui/button";
@@ -20,17 +20,18 @@ import { FormError } from "@/components/form-error";
 import {FormSuccess} from "@/components/form-success";
 
 
-import { login } from "@/actions/login";
+import { register } from "@/actions/register";
 
  const LoginForm = () => {
     const [error, setError] = useState<string | undefined>("");
     const [success, setSuccess] = useState<string | undefined>("");
     const [isPending, setPending]=useTransition();
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
         defaultValues: {
             email: "",
             password: "",
+            name:""
         },
     });
 
@@ -40,10 +41,10 @@ import { login } from "@/actions/login";
     });
     
 
-const onSubmit = (values:z.infer<typeof LoginSchema>)=>{
+const onSubmit = (values:z.infer<typeof RegisterSchema>)=>{
     setError("");
     setSuccess("");
-    setPending(()=>{login(values)
+    setPending(()=>{register(values)
     .then((data)=>{
         setError(data.error);
         setSuccess(data.success);
@@ -53,14 +54,35 @@ const onSubmit = (values:z.infer<typeof LoginSchema>)=>{
 })}
 
     return ( <CardWrapper
-    headerLabel="Welcome Back"
-    backButtonLabel="Don't have an account?"
-    backButtonHref="/auth/register"
+    headerLabel="Create an Account"
+    backButtonLabel="Already have an Account?"
+    backButtonHref="/auth/login"
     showSocial>
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)}
                 className="space-y-6">
                     <div className="space-y-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({field})=>(
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                <Input
+                                disabled={isPending}
+                                {...field}
+                                type="text"
+                                placeholder="John Doe"
+                                />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                            
+                        )}
+                        />
+
+
                         <FormField
                         control={form.control}
                         name="email"
@@ -102,7 +124,7 @@ const onSubmit = (values:z.infer<typeof LoginSchema>)=>{
                     </div>
                 <FormError message={error}/>
                 <FormSuccess message={success}/>
-                <Button disabled={isPending} type="submit" className="w-full">Login</Button>
+                <Button disabled={isPending} type="submit" className="w-full">Sign Up</Button>
             </form>
         </Form>
     </CardWrapper> );
